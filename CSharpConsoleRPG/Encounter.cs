a91ec2340d704bcfc934f0af39be025c13d56d2c
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-02-26 17:09:22 +0100

    Tried to create the gamesession and encounters
    Basis for gamesession and encounter and npcs. Trying to use inheritance

---
+++ CSharpConsoleRPG/Encounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleRPG
+{
+    internal class Encounter
+    {
+        public int EncounterType { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enemy"></param>
+        /// <param name="random"></param>
+        /// <param name="player"></param>
+        public Encounter(Enemy enemy, Random random, Player player) 
+        {
+            
+        }
+
+    }
+}
--- CSharpConsoleRPG/GameSession.cs
+++ CSharpConsoleRPG/GameSession.cs
@@ -8,5 +8,23 @@ namespace CSharpConsoleRPG
 {
     internal class GameSession
     {
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+
+        public GameSession() 
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.PlayerName = "Christiaan";
+            CurrentPlayer.PlayerHealth = 100;
+            CurrentPlayer.PlayerLevel = 1;
+            CurrentPlayer.PlayerExperience = 1;
+            CurrentPlayer.PlayerGold = 0;
+
+            CurrentLocation = new Location();
+            CurrentLocation.LocationName = "Home";
+            CurrentLocation.XCoordinate= 0;
+            CurrentLocation.YCoordinate= 0;
+            CurrentLocation.LocationDescription = "Description";
+        }
     }
 }
