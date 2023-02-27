8c3b74a1fa92a79d72a432e2f93cfb893f8aaedf
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-02-27 14:03:20 +0100

    Encoutner class updated
    Put the core battle loop into the encounter class and called it from the main program.

--- CSharpConsoleRPG/Encounter.cs
+++ CSharpConsoleRPG/Encounter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CSharpConsoleRPG
 {
-    internal class Encounter
+    public class Encounter
     {
         public int EncounterType { get; set; }
 
@@ -18,7 +18,57 @@ namespace CSharpConsoleRPG
         /// <param name="player"></param>
         public Encounter(Enemy enemy, Random random, Player player) 
         {
-            
+            Console.WriteLine(player.PlayerName + " , you have encountered " + enemy.Name + "! What would you like to do?");
+
+
+            // While the first enemy is NOT dead and while the player is NOT dead, continue this combat.
+            while (!enemy.IsDead && !player.PlayerIsDead)
+            {
+                Console.WriteLine("1. Single Attack.");
+                Console.WriteLine("2. A Three Strike Attack.");
+                Console.WriteLine("3. Defend.");
+                Console.WriteLine("4. Heal youreslf.");
+
+                int playerChoice = Convert.ToInt32(Console.ReadLine());
+
+                switch (playerChoice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("You have attacked the " + enemy + "! How cruel!");
+                            enemy.TakesDamage(random.Next(1, 15));
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("You have attacked the " + enemy + ", three times! How mean!");
+                            for (int i = 0; i < 3; i++)
+                            {
+                                enemy.TakesDamage(random.Next(1, 5));
+
+                            }
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("The " + enemy + " does nothing. It is worried about you trying to defend.");
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("You are choosing to heal yourself.");
+                            player.Heal(random.Next(5, 20));
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("This is not a valid input. The only valid input is an integer that is either 1, 2 or 3.");
+                        }
+
+                        break;
+                }
+                player.PlayerTakesDamage(random.Next(1, 10));
+            }
         }
 
     }
--- CSharpConsoleRPG/Program.cs
+++ CSharpConsoleRPG/Program.cs
@@ -12,67 +12,24 @@
 
             //Asks the player for their name and stores the name inside the object player.
             Console.WriteLine("What's your name?");
-            player.PlayerName = Console.ReadLine();
+            try
+            {
+                //The ! removes the possible error message, but not the error. Just done as a test for both the ! and the error.
+                player.PlayerName = Console.ReadLine()!;
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine("You forgot to write a name, this results in this error: " + a.Message);
+                throw;
+            }
+            
 
             //Welcoming the playre and using their name.
             Console.WriteLine("Welcome " + player.PlayerName + ". We're glad to see you!");
 
-
-            //First playre encounter.
-            Enemy firstEnemy = new Enemy("Giant Slime");
-
-            Console.WriteLine(player.PlayerName + " , you have encountered " + firstEnemy.Name + "! What would you like to do?");
-
+            Enemy enemy = new Enemy("Enemy Dave");
             
-            // While the first enemy is NOT dead and while the player is NOT dead, continue this combat.
-            while (!firstEnemy.IsDead && !player.PlayerIsDead)
-            {
-                Console.WriteLine("1. Single Attack.");
-                Console.WriteLine("2. A Three Strike Attack.");
-                Console.WriteLine("3. Defend.");
-                Console.WriteLine("4. Heal youreslf.");
-
-                int playerChoice = Convert.ToInt32(Console.ReadLine());
-
-                switch (playerChoice)
-                {
-                    case 1:
-                        {
-                            Console.WriteLine("You have attacked the slime! How cruel!");
-                            firstEnemy.TakesDamage(random.Next(1, 15));
-                        }
-                        break;
-                    case 2:
-                        {
-                            Console.WriteLine("You have attacked the slime, three times! How mean!");
-                            for (int i = 0; i < 3; i++)
-                            {
-                                firstEnemy.TakesDamage(random.Next(1, 5));
-
-                            }
-                        }
-                        break;
-                    case 3:
-                        {
-                            Console.WriteLine("The slime does nothing. It is worried about you trying to defend.");
-                        }
-                        break;
-                    case 4:
-                        {
-                            Console.WriteLine("You are choosing to heal yourself.");
-                            player.Heal(random.Next(5, 20));
-                        }
-                        break;
-                    default:
-                        {
-                            Console.WriteLine("This is not a valid input. The only valid input is an integer that is either 1, 2 or 3.");
-                        }
-
-                        break;
-                }
-                player.PlayerTakesDamage(random.Next(1, 10));
-            }
-
+            Encounter firstEncounter = new Encounter(enemy, random, player);
 
 
             Console.WriteLine("Game over.");
