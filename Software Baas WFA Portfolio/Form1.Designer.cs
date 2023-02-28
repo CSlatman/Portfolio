66f0728431a855ee5574e3ebaf712db806010ef7
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-02-28 08:11:14 +0100

    Simple cm to inch converter
    Made a simple centimeter to inch converter. It does what it's supposed to, but haven't been able to show the result in a pop up yet., though a form does pop up.

---
+++ Software Baas WFA Portfolio/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Software_Baas_WFA_Portfolio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtBoxOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input your height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "cm";
+            // 
+            // txtBoxInput
+            // 
+            this.txtBoxInput.Location = new System.Drawing.Point(234, 121);
+            this.txtBoxInput.Name = "txtBoxInput";
+            this.txtBoxInput.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxInput.TabIndex = 2;
+            this.txtBoxInput.TextChanged += new System.EventHandler(this.txtBoxInput_TextChanged);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(422, 119);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtBoxOutput
+            // 
+            this.txtBoxOutput.Location = new System.Drawing.Point(233, 232);
+            this.txtBoxOutput.Name = "txtBoxOutput";
+            this.txtBoxOutput.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxOutput.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "inches";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxOutput);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtBoxInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Height calculator tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtBoxInput;
+        private Button btnConvert;
+        private TextBox txtBoxOutput;
+        private Label label3;
+    }
+}
---
+++ Software Baas WFA Portfolio/Form1.cs
@@ -0,0 +1,40 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Software_Baas_WFA_Portfolio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // On click, convert cm to feet from the input.
+
+            //CentimeterInput is a decimal variable.
+            //Converts the string to a decimal (from the textchange txtBoxInput upon the button click)
+            string CentimeterInput = (txtBoxInput.Text);
+            double CentimeterOutput;
+            double.TryParse(CentimeterInput, out CentimeterOutput);
+
+            //Now we apply the centimeter to inches ratio which is 1 cm for 0,393701 inch
+            double inchvalue = 0.393701;
+            double InchOutput = (CentimeterOutput * inchvalue);
+
+            txtBoxOutput.Text = InchOutput.ToString();
+
+            var inchForm = new Form();
+            inchForm.Show(this); // if you need non-modal window
+
+            
+
+        }
+    }
+}
