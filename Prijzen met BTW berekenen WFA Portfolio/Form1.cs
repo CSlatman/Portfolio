9839c22577df8131607d78f29ee76fd24d0b7b74
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-02-28 12:01:10 +0100

    created calculating VAT program
    Created teh VAT program for the portfolio in a Windows Forms App

---
+++ Prijzen met BTW berekenen WFA Portfolio/Form1.cs
@@ -0,0 +1,10 @@
+namespace Prijzen_met_BTW_berekenen_WFA_Portfolio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
