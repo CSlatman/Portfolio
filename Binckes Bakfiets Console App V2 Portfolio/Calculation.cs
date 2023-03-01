f8f3e5dd11f6961c16f6348bfa3ef2519be42e95
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-03-01 14:03:24 +0100

    Created the binckes bakfiets main project solution

---
+++ Binckes Bakfiets Console App V2 Portfolio/Calculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binckes_Bakfiets_Console_App_V2_Portfolio
+{
+    internal class Calculation
+    {
+    }
+}
---
+++ Binckes Bakfiets Console App V2 Portfolio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Binckes_Bakfiets_Console_App_V2_Portfolio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
