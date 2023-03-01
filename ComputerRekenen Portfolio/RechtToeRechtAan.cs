d20d54519eed2d5b5e5b6db0048bdbfa84dc534d
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-03-01 16:05:33 +0100

    First aspect of double assignment

--- ComputerRekenen Portfolio/RechtToeRechtAan.cs
+++ ComputerRekenen Portfolio/RechtToeRechtAan.cs
@@ -12,6 +12,25 @@ namespace ComputerRekenen_Portfolio
         //ctor for RechtToeRechtAan
         public RechtToeRechtAan()
         {
+            // Need to solve 3 assignments
+
+            // Assignment 1
+            //  100 x 0,7 - 25 x 0,6 = 127.857142857
+            // Calc answer
+
+            double ass1 = ((100 * 0, 7) - (25 * 0, 6));
+
+            // Assignment 2
+            //  18.0 / 5.0 = 3.6
+            // Calc answer
+
+
+
+            // Assignment 3
+            //  100 / 30 = 3.333333333
+            // Calc answer
+
+
 
         }
     }
