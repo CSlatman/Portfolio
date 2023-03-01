805948a17777dfc997d5d198f801e478432d02f0
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-03-01 14:39:23 +0100

    ComputerRekenen created
    Created the 5 assignments within ComputerRekenen as classes and made the constructors

---
+++ ComputerRekenen Portfolio/Een123tje.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerRekenen_Portfolio
+{
+    public class Een123tje
+    {
+        // ctor for Een123tje
+        public Een123tje()
+        {
+
+        }
+    }
+}
---
+++ ComputerRekenen Portfolio/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ComputerRekenen_Portfolio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5 assignments
+
+            // 1. Recht-Toe-Recht-Aan
+
+
+            // 2. Al Wandelend...
+
+
+            // 3. Ik een beetje maar dan jij...
+
+
+            // 4. Een 1-2-3-tje
+
+
+            // 6. Hoeveel paldindromen kan ik maken met ...?
+
+
+
+        }
+    }
+}
