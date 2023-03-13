3073a432b0b4f0af7b11d5b782ce7efee9992977
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-03-13 18:54:26 +0100

    Created Solution
    Created the Student Class and the fields and constructor

---
+++ Student Admin Portfolio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Admin_Portfolio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Student Admin Portfolio/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Admin_Portfolio
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName 
+        { 
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Student(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+}
