59258c3e794e2ddb705085d55ca473c32aaf4d26
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-03-20 18:15:54 +0100

    new program

---
+++ WinFormsApp1HerovMonster/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1HerovMonster
+{
+    internal class Monster
+    {
+        private Random randomizer = new Random();
+
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+
+        public Monster(string name, int attack)
+        {
+            Name = name;
+            Attack = attack;
+            Health = 100;
+
+
+
+        }
+
+    }
+}
