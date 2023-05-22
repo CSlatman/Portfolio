dde084ef3d907618b6284bd2bf44dd8cf84e60dd
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-05-22 18:52:19 +0200

    Updated Menu
    Updated Menu with Switch statement

--- Binckes Bakfiets Console App V2 Portfolio/Menu.cs
+++ Binckes Bakfiets Console App V2 Portfolio/Menu.cs
@@ -14,32 +14,30 @@ namespace Binckes_Bakfiets_Console_App_V2_Portfolio
         {
             // This sets hte MenuChoice to be 0, in case the user doesn't want to enter the menu.
             int MenuChoice = 0;
-            Console.WriteLine("Welcome to the Binckes Bakfiets application.");
-            Console.WriteLine("Do you wish to enter the menu, then press 1.");
+            //Welcomes the user to the BB Menu.
+            Console.WriteLine("This is the Binckes Bakfiets Menu!");
+
+
+            Console.WriteLine();
+
+            //Informs the user about the sequence in which the program asks questions.
+            Console.WriteLine("This program is sequenced in a way that you will choose which bicycle you would like first.");
+            Console.WriteLine("Second, you will select the relevant accessories you deem appropriate.");
+            Console.WriteLine("Third and last, we'll ask you how long you want to rent it,");
+            Console.WriteLine(" whereupon you can see the total and confirm or decline the transaction.");
 
             string input = Console.ReadLine();
             Int32.TryParse(input, out MenuChoice);
 
-            while (MenuChoice == 1)
+            switch (MenuChoice)
             {
-                //Welcomes the user to the BB Menu.
-                Console.WriteLine("This is the Binckes Bakfiets Menu!");
-                
+                case 0:
+                    Console.WriteLine("Error. This is an invalid input. MenuChoice is set to 0.");
+                    break;
+                case 1:
+                    Console.WriteLine("You have chosen poorly.");
+                    break;
 
-                Console.WriteLine();
-
-                //Informs the user about the sequence in which the program asks questions.
-                Console.WriteLine("This program is sequenced in a way that you will choose which bicycle you would like first.");
-                Console.WriteLine("Second, you will select the relevant accessories you deem appropriate.");
-                Console.WriteLine("Third and last, we'll ask you how long you want to rent it,");
-                Console.WriteLine(" whereupon you can see the total and confirm or decline the transaction.");
-
-
-
-                // This increases the before entered MenuChoice by 1, which then makes the while loop false.
-                MenuChoice++;
-                break;
-                
 
 
 
