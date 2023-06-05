579d1f1b57f4d0c440bcfcc7965da75275d73f0e
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-06-05 19:16:50 +0200

    ensured branch to main

--- JSONtestapp/Program.cs
+++ JSONtestapp/Program.cs
@@ -2,7 +2,6 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Newtonsoft.Json;
 
 
 namespace JSONtestapp
@@ -11,22 +10,10 @@ namespace JSONtestapp
     {
         static void Main(string[] args)
         {
-            //List<Customer> customers = new List<Customer>();
-            //customers.Add(new Customer { Name = "Jason", Age = 25 });
-            //customers.Add(new Customer { Name = "Nikki", Age = 20 });
+
             Customer customer = new Customer("Nikki", 25);
             
-<<<<<<< Updated upstream
-            string json = JsonConvert.SerializeObject(customers, Formatting.Indented);
-            string fileName = "customers.json";
-            File.WriteAllText(fileName, json);
 
-            string jsonString = File.ReadAllText(fileName);
-            Console.WriteLine(jsonString);
-
-            Customer customerlist = JsonConvert.DeserializeObject<Customer>(jsonString);
-            
-=======
             var options = new JsonSerializerOptions { WriteIndented = true };
 
             string json = JsonSerializer.Serialize(customer);
@@ -42,7 +29,7 @@ namespace JSONtestapp
 
             Console.WriteLine($"Name: {customer1.Name}");
             Console.WriteLine($"Age: {customer1.Age}");
->>>>>>> Stashed changes
+
         }
     }
 }
