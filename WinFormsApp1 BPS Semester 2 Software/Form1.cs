51a7010be9d23747c8de2c2d58531042128d0496
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-09-10 20:26:06 +0200

    webshop BPS

---
+++ WinFormsApp1 BPS Semester 2 Software/Form1.cs
@@ -0,0 +1,10 @@
+namespace WinFormsApp1_BPS_Semester_2_Software
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
