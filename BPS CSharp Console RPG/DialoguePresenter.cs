bf483d00ca5c2272969ba944d32eae94ae6c4137
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-09-14 18:46:18 +0200

    Create DialoguePresenter

---
+++ BPS CSharp Console RPG/DialoguePresenter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPS_CSharp_Console_RPG
+{
+    internal class DialoguePresenter
+    {
+    }
+}
