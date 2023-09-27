8c63c4e5e574915994e5509b31f2b19fe9668fe6
Christiaan Slatman <Slatman_christiaan@hotmail.com>
2023-09-27 19:08:50 +0200

    Update Program.cs

--- Testproject/Program.cs
+++ Testproject/Program.cs
@@ -7,31 +7,21 @@ namespace Testproject
     {
         static void Main(string[] args)
         {
+            int j = 2;
+            for (int i = 1; i < 100;
+                 i = i * 2)
+            {
+                j = j - 1;
+                while (j < 25)
+                {
+                    // How many times will
+                    // the next statement
+                    // be executed? 7 times
+                    j = j + 5;
+                    Console.WriteLine(j);
+                }
+            }
 
-            OperatorExamples();
-
-
-        }
-
-        private static void OperatorExamples()
-        {
-            // This statement declares a variable and sets it to 3
-            int width = 3;
-
-            // The ++ operator increments a variable (adds 1 to it)
-            width++;
-
-            // Declare two more int variables to hold numbers and
-            // use the + and * operators to add and multiply values
-            int height = 2 + 4;
-            int area = width * height;
-            Console.WriteLine(area);
-
-            // The next two statements declare string variables
-            // and use + to concatenate them (join them together)
-            string result = "The area";
-            result = result + " is " + area;
-            Console.WriteLine(result);
         }
     }
 }
